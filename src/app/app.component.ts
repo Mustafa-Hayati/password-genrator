df5b98636488fa43d959f4ebea6ea2b8b9683f96
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-25 13:32:25 +0330

    Bind the appropriate methods to the lettre, number and symbol inputs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   password = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick(): void {
     this.password = Math.random().toString();
+  }
+
+  onLettersChange(): void {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onNumbersChange(): void {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onSymbolsChange(): void {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
